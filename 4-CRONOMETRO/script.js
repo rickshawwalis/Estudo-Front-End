9c84ac8321e91e910ea76beb7c9e810120667f9a
rickshawwalis <rickshawwallis@gmail.com>
2023-04-14 21:26:19 -0300

    y

--- 4-CRONOMETRO/script.js
+++ 4-CRONOMETRO/script.js
@@ -1,4 +1,4 @@
-function criaHoraDosSegundos(segundos) {
+function criaHoraDosSegundos(segundos) {//dar o formato do cronometo, exemplo: 00:00:00
   const data = new Date(segundos * 1000);
   return data.toLocaleTimeString('pt-BR', {
       hour12: false,
@@ -8,10 +8,10 @@ function criaHoraDosSegundos(segundos) {
 
 const tempo = document.querySelector("#tempo");
 let segundos = 0;
-let timer;
+let timer
 
-function iniciarRelogio() {
-  timer = setInterval(function () {
+function iniciarRelogio() {//responsável por existir a contagem
+ timer = setInterval(function () {
       segundos++;
       tempo.innerHTML = criaHoraDosSegundos(segundos);
   }, 1000);
@@ -39,50 +39,3 @@ document.addEventListener('click', function (e) {
   }
 });
 tempo.style.textAlign = 'center';
-
-/*const tempo = document.querySelector("#tempo");
-const iniciar = document.querySelector("#iniciar");
-const pausar = document.querySelector("#pausar");
-const zerar = document.querySelector("#zerar");
-
-let hours = 0;
-let minutes = 0;
-let seconds = 0;
-
-let c = () => { // A função c é responsável por atualizar o temporizador a cada segundo.
-  seconds++; //A primeira linha da função c incrementa a variável seconds em 1 a cada vez que a função é chamada.
-  if (seconds == 60) {//if verifica se seconds atingiu 60
-    minutes++;//se Second atingir 60s, a variável minutes é incrementada em 1
-    seconds = 0; //variável seconds é reinicializada para 0
-  }
-  if (minutes == 60) {//if verifica se minutes atingiu 60
-    hours++;//se minute atingir 60min, a variável hours é incrementada em 1
-    minutes = 0;//variável minutes é reinicializada para 0
-  }
-  tempo.innerHTML =
-    `${hours.toString().padStart(2, '0')} : ${minutes.toString().padStart(2, '0')} : ${seconds.toString().padStart(2, '0')}`;
-};
-
-let timer;
-
-iniciar.addEventListener("click", () => {//iniciar
-  clearInterval(timer);
-  timer = setInterval(c, 1000);
-  tempo.style.color = "black";//tempo pq é ligado a id de onde aparece o horário no html
-});
-
-pausar.addEventListener("click", () => {//pausar
-  clearInterval(timer);
-  tempo.style.color = "red";
-});
-
-zerar.addEventListener("click", () => {//zear
-  clearInterval(timer);
-  hours = 0;
-  minutes = 0;
-  seconds = 0;
-  tempo.innerHTML = "00 : 00 : 00";
-  tempo.style.color = "black";//tempo pq é ligado a id de onde aparece o horário no html
-});
-tempo.style.textAlign = 'center';//centraliza o texto da id que aparece o horário no html
-*/
