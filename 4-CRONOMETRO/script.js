f2481d4f8e5c14fecf6f9c6db8bba9fdfdaf2821
rickshawwalis <rickshawwallis@gmail.com>
2023-04-14 23:05:12 -0300

    g

--- 4-CRONOMETRO/script.js
+++ 4-CRONOMETRO/script.js
@@ -1,8 +1,8 @@
 function criaHoraDosSegundos(segundos) {//dar o formato do cronometo, exemplo: 00:00:00
   const data = new Date(segundos * 1000);
   return data.toLocaleTimeString('pt-BR', {
-      hour12: false,
-      timeZone: 'UTC',
+    hour12: false,
+    timeZone: 'UTC',
   });
 }
 
@@ -11,9 +11,9 @@ let segundos = 0;
 let timer
 
 function iniciarRelogio() {//responsável por existir a contagem
- timer = setInterval(function () {
-      segundos++;
-      tempo.innerHTML = criaHoraDosSegundos(segundos);
+  timer = setInterval(function () {
+    segundos++;
+    tempo.innerHTML = criaHoraDosSegundos(segundos);
   }, 1000);
 }
 
@@ -21,21 +21,21 @@ document.addEventListener('click', function (e) {
   const el = e.target;
 
   if (el.classList.contains("iniciar")) {
-      clearInterval(timer);
-      iniciarRelogio();
-      tempo.style.color = "black";
+    clearInterval(timer);
+    iniciarRelogio();
+    tempo.style.color = "black";
   }
 
   if (el.classList.contains("pausar")) {
-      clearInterval(timer);
-      tempo.style.color = "red";
+    clearInterval(timer);
+    tempo.style.color = "red";
   }
 
   if (el.classList.contains("zerar")) {
-      clearInterval(timer);
-      tempo.innerHTML = '00:00:00';
-      segundos = 0;
-      tempo.style.color = "black";
+    clearInterval(timer);
+    tempo.innerHTML = '00:00:00';
+    segundos = 0;
+    tempo.style.color = "black";
   }
 });
 tempo.style.textAlign = 'center';
